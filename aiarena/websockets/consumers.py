8c140982c1662759163aa0775373ca405b0b2ec2
lladdy <randallboland@bigpond.com>
2019-10-15 17:10:25 +1030

    Add websockets HeartbeatConsumer

--- aiarena/websockets/consumers.py
+++ aiarena/websockets/consumers.py
@@ -18,3 +18,21 @@ class ChatConsumer(WebsocketConsumer):
         self.send(text_data=json.dumps({
             'message': message
         }))
+
+
+class HeartbeatConsumer(WebsocketConsumer):
+    """
+    Option 1: Rely on the in-built websocket heartbeat.
+    Need to find out when disconnect is fired and whether this will server our purpose.
+    If disconnect is fired when a websocket times out, when this could be our heartbeat.
+    Option 2: use message sending as our heartbeat - a reliable fallback.
+    """
+    # todo: ensure arena clients can only register a heartbeat for matches they're assigned to.
+    def connect(self):
+        self.accept()
+
+    def disconnect(self, close_code):
+        pass
+
+    def receive(self, text_data=None, bytes_data=None):
+        pass
