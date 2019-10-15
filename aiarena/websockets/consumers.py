8b13d7d31b56238b37721564e7864e6fa53c3c95
danielvschoor <43602193+danielvschoor@users.noreply.github.com>
2019-10-15 20:25:54 +0200

    Added post_save signal handler

--- aiarena/websockets/consumers.py
+++ aiarena/websockets/consumers.py
@@ -1,6 +1,9 @@
 import json
 
 from channels.generic.websocket import WebsocketConsumer
+from ..core.models import Result, Match
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 
 class ChatConsumer(WebsocketConsumer):
@@ -27,12 +30,21 @@ class HeartbeatConsumer(WebsocketConsumer):
     If disconnect is fired when a websocket times out, when this could be our heartbeat.
     Option 2: use message sending as our heartbeat - a reliable fallback.
     """
+    match_id = 0
     # todo: ensure arena clients can only register a heartbeat for matches they're assigned to.
     def connect(self):
         self.accept()
+        match_id = self.scope['url_route']['kwargs']['match_id']
 
     def disconnect(self, close_code):
         pass
 
     def receive(self, text_data=None, bytes_data=None):
-        pass
+        if text_data is not None and text_data == 'ping':
+            self.send(text_data='pong')
+        
+@receiver(post_save, sender=Result)
+def result_signal_handler(sender, instance, **kwargs):
+    if instance.type == 'MatchCancelled':
+        pass ##Cancel the match on the client
+
